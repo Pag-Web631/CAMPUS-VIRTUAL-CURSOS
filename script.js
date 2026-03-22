54a9483614af947955aac6dcf0f341eec9d64293
Pag-Web22 <yordaysanchez09@gmail.com>
2026-03-22 13:28:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,67 @@
 let user = null;
 
+// LOGIN
+function login(){
+    const userInput = document.getElementById("loginUser").value;
+    const pass = document.getElementById("loginPass").value;
+
+    const saved = JSON.parse(localStorage.getItem("usuario"));
+
+    if(!saved || saved.user !== userInput || saved.pass !== pass){
+        alert("Datos incorrectos");
+        return;
+    }
+
+    if(saved.rol === "docente") location.href = "docente.html";
+    else location.href = "estudiante.html";
+}
+
+// REGISTRO
+function registrar(){
+    const user = document.getElementById("regUser").value;
+    const pass = document.getElementById("regPass").value;
+    const rol = document.getElementById("regRol").value;
+
+    if(!user || !pass || !rol){
+        alert("Completa todo");
+        return;
+    }
+
+    localStorage.setItem("usuario", JSON.stringify({
+        nombre: user,
+        user,
+        pass,
+        rol
+    }));
+
+    alert("Registrado");
+}
+
+// PANEL USUARIO
 function cargarUsuario(){
     const data = localStorage.getItem("usuario");
 
     if(!data){
-        window.location.href = "index.html";
+        location.href = "index.html";
         return;
     }
 
     user = JSON.parse(data);
 
-    // Mostrar nombre
-    const bienvenida = document.getElementById("bienvenida");
-    if(bienvenida){
-        bienvenida.innerText = "Bienvenido, " + user.nombre;
-    }
-
-    // Aplicar estilo por rol
     document.body.classList.add(user.rol);
+
+    const b = document.getElementById("bienvenida");
+    if(b) b.innerText = "Bienvenido " + user.nombre;
 }
+
+// LOGOUT
 function logout(){
     localStorage.removeItem("usuario");
-    window.location.href = "index.html";
+    location.href = "index.html";
+}
+
+// MOSTRAR REGISTRO
+function toggleRegistro(){
+    const reg = document.getElementById("registro");
+    reg.style.display = reg.style.display === "none" ? "block" : "none";
 }
